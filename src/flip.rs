61a9d3474f39317f27131b3ea8d90d9b1b7bed4f
DreamySkrell <>
2022-11-26 18:56:02 +0100

    f

--- src/flip.rs
+++ src/flip.rs
@@ -12,6 +12,43 @@ fn flip() {
             for row in &mut parsed.rows {
                 row.tiles.reverse();
             }
+
+            for prototype in &mut parsed.prototypes {
+                for atom in &mut prototype.atoms {
+                    // pixel shift
+                    if let Some(VarVal::Int(pixel_y)) = atom.vars.get_mut("pixel_y") {
+                        *pixel_y = *pixel_y * -1.0;
+                    }
+
+                    // cables
+                    {
+                        if let Some(VarVal::String(icon_state)) = atom.vars.get_mut("icon_state") {
+                            if icon_state == "2-4" {
+                                *icon_state = "1-4".into();
+                                atom.vars["d1"] = VarVal::Int(1.0);
+                            }
+                        }
+                        if let Some(VarVal::String(icon_state)) = atom.vars.get_mut("icon_state") {
+                            if icon_state == "1-4" {
+                                *icon_state = "2-4".into();
+                                atom.vars["d1"] = VarVal::Int(2.0);
+                            }
+                        }
+                        if let Some(VarVal::String(icon_state)) = atom.vars.get_mut("icon_state") {
+                            if icon_state == "2-4" {
+                                *icon_state = "1-4".into();
+                                atom.vars["d1"] = VarVal::Int(2.0);
+                            }
+                        }
+                        if let Some(VarVal::String(icon_state)) = atom.vars.get_mut("icon_state") {
+                            if icon_state == "1-8" {
+                                *icon_state = "2-8".into();
+                                atom.vars["d1"] = VarVal::Int(2.0);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         let printed = print(&parsed);
